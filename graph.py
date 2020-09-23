6a0d13437b4df176e5d83538cfe7daf7ca387d1a
RobertSparadrap <robert.harakaly@epitech.eu>
2020-09-23 11:43:08 +0200

    average and compare

--- graph.py
+++ graph.py
@@ -13,6 +13,7 @@ def nb_data():
         if run == "###########################################################\n":
             break
         i += 1
+    runtime.close()
     return i - 2
 
 # To take all the data of the Demo
@@ -28,9 +29,9 @@ def take_data_of_the_demo(runtime, x):
     return data_run
 
 # Search the Data Demo
-def search_loop(runtime, demo, data):
+def search_loop(runtime, demo, demo2, data):
     while 1:
-        if data == demo+'\n':
+        if data == demo+'\n' or data == demo2+'\n':
             runtime.readline()
             break
         data = runtime.readline()
@@ -39,12 +40,13 @@ def search_loop(runtime, demo, data):
 # Take the Data Demo
 def take_demo_time(i, x):
     demo = sys.argv[1]
+    demo2 = sys.argv[2]
     runtime = open("runtime.txt")
     j = 0
-    while j < 1+i*(x+3)*2:
+    while j < 1+i*(x+7)*2:
         data = runtime.readline()
         j += 1
-    data = search_loop(runtime, demo, data)
+    data = search_loop(runtime, demo, demo2, data)
     data_run = take_data_of_the_demo(runtime, x)
     runtime.close()
     return data_run
@@ -63,33 +65,72 @@ def take_data_CPU(data_run, x):
 def nb_runtimes():
     try:
         demo = sys.argv[1]
+        demo2 = sys.argv[2]
     except:
-        print("Choose a Demo:\ncell_division\ndiffusion\ngene_regulation\nmultiple_simulations\nsom_clustering\ntumor_concept")
+        print("Choose a Demo:\nsoma_clustering\ntumor_concept")
         exit()
     runtime = open("runtime.txt")
     data = runtime.readline()
     i = 0
     while 1:
-        if data == demo+'\n':
+        if data == demo+'\n' or data == demo2+'\n':
             runtime.readline()
             i += 1
         if data == "":
             if i == 0:
-                print("Choose a Demo:\ncell_division\ndiffusion\ngene_regulation\nmultiple_simulations\nsom_clustering\ntumor_concept")
+                print("Choose a Demo:\nsoma_clustering\ntumor_concept")
                 exit()
             return i
         data = runtime.readline()
     return i
 
-i = nb_runtimes()
-j = 0
-x = 1
-y = 0
-while j < i:
-    x = nb_data()
-    data_run = take_demo_time(j, x)
-    data_CPU = take_data_CPU(data_run, x)
-    plt.plot(range(x), data_CPU, 'b--')
-    j += 1
-plt.xlabel(sys.argv[1])
-plt.show()
+# Graph to directly compare Data
+def compare(i):
+    j = 0
+    while j < i:
+        x = nb_data()
+        print(i)
+        print(x)
+        data_run = take_demo_time(j, x)
+        data_CPU = take_data_CPU(data_run, x)
+        print(data_CPU)
+        plt.plot(range(x), data_CPU, 'b--')
+        j += 1
+    return
+
+# Graph to compare the average Data
+def average(i):
+    j = 0
+    moy = [0] * i
+    while j < i:
+        tmp = 0
+        h = 0
+        x = nb_data()
+        data_run = take_demo_time(j, x)
+        data_CPU = take_data_CPU(data_run, x)
+        while h < x:
+            tmp = data_CPU[h] + tmp
+            h += 1
+        moy[j] = tmp / x
+        j += 1
+    plt.plot(range(i), moy, 'r--')
+    return
+
+# main program
+def main():
+    i = nb_runtimes()
+    x = 1
+    y = 0
+    try:
+        if sys.argv[2] == "--compare" or sys.argv[2] == "-c" or sys.argv[1] == "--compare" or sys.argv[1] == "-c":
+            compare(i)
+        if sys.argv[2] == "--average" or sys.argv[2] == "-a" or sys.argv[1] == "--average" or sys.argv[1] == "-a":
+            average(i)
+    except:
+        print("Do you want to compare data or to compare average data ?")
+        exit()
+    plt.xlabel(sys.argv[1])
+    plt.show()
+
+if __name__ == "__main__":
+    main()
