3a603ee20d3188c1818cf0dfb535794086bd50ec
RobertSparadrap <robert.harakaly@epitech.eu>
2020-09-25 09:19:50 +0200

    change branch

--- my_bensh.sh
+++ my_bensh.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+if [[ $1 == "" ]]; then
+    echo ./my_bensh [number_of_iteration] [branch/NULL_if_use_branch_master]
+    exit
+fi
+
+if [[ $2 != "" ]]; then
+    cd /home/bdm/biodynamo/
+    git checkout $2
+    cd build/
+    make clean
+    cmake ../
+    make
+    cd
+    cd demo
+fi
+
 FILES=*
 for i in $FILES; do
     if [[ $i != "soma_clustering" && $i != "soma_clustering_2" && $i != "tumor_concept" ]]; then
