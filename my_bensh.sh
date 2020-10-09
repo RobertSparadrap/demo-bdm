5f08439b3f2462e36a9fde9c1a7f40283277aa74
RobertSparadrap <robert.harakaly@epitech.eu>
2020-10-09 14:36:32 +0200

    remove some folder

--- my_bensh.sh
+++ my_bensh.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
 #source /home/bdm/biodynamo/build/bin/thisbdm.sh
-if [[ $1 == "" ]]; then
-    echo ./my_bensh [number_of_iteration] [branch/NULL_if_use_branch_master]
-    exit
-fi
+# if [[ $1 == "" ]]; then
+#     echo ./my_bensh [number_of_iteration] [branch/NULL_if_use_branch_master]
+#     exit
+# fi
 
-if [[ $2 != "" ]]; then
-    cd /home/bdm/biodynamo/
-    git checkout $2
-#    git log | awk '/Date/ {print $3, $4, $5, $6}' | awk "NR==1{print;exit}" > ../demo/date.txt
-    cd build/
-    make clean
-    cmake ../
-    make
-    cd
-    cd demo
-#    if [[ compare_date == 1 ]]; then
-#    if [[ $2 == "b4871f1b8321daf6c73dfe1a70cd6b0e00bdb05e" ]]; then
-#        echo "change branche"
-#        git checkout old-soma_clustering
-#    fi
-fi
+# if [[ $2 != "" ]]; then
+#     cd /home/bdm/biodynamo/
+#     git checkout $2
+# #    git log | awk '/Date/ {print $3, $4, $5, $6}' | awk "NR==1{print;exit}" > ../demo/date.txt
+#     cd build/
+#     make clean
+#     cmake ../
+#     make
+#     cd
+#     cd demo
+# #    if [[ compare_date == 1 ]]; then
+# #    if [[ $2 == "b4871f1b8321daf6c73dfe1a70cd6b0e00bdb05e" ]]; then
+# #        echo "change branche"
+# #        git checkout old-soma_clustering
+# #    fi
+# fi
 
 FILES=*
 for i in $FILES; do
